db2d8c07dc57bc9b85e3c199b477516b40f90a0c
Skorpien <30352529+Skorpien@users.noreply.github.com>
2020-01-05 18:38:01 +0100

    Module 22.2

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -8,9 +8,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Component
 public class TrelloClient {
@@ -24,22 +22,31 @@ public class TrelloClient {
     @Value("${trello.app.token}")
     private String trelloToken;
 
+    @Value("${trello.app.username}")
+    private String trelloUsername;
 
     @Autowired
     private RestTemplate restTemplate;
 
     public List<TrelloBoardDto> getTrelloBoards() {
 
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/_skorpien_/boards")
-                .queryParam("key", trelloAppKey)
-                .queryParam("token", trelloToken)
-                .queryParam("fields", "name,id").build().encode().toUri();
+        URI url = buildUri("fields", "name,id");
 
         TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
 
-        if (boardsResponse != null) {
-            return Arrays.asList(boardsResponse);
-        }
-        return new ArrayList<>();
+        return Optional.of(Arrays.asList(Objects.requireNonNull(boardsResponse))).orElse(new ArrayList<>());
+
+     //   if (boardsResponse != null) {
+    //       return Arrays.asList(boardsResponse);
+    //    }
+        //return new ArrayList<>();
+
+    }
+
+    private URI buildUri (String name, String object) {
+        return UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUsername + "/boards")
+               .queryParam("key", trelloAppKey)
+               .queryParam("token", trelloToken)
+               .queryParam(name, object).build().encode().toUri();
     }
 }
