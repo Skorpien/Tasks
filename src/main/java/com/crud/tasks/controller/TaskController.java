c74bd619d617fe18186e0cd2631798569df6e728
Skorpien <30352529+Skorpien@users.noreply.github.com>
2020-03-02 23:01:00 +0100

    Module 29.3

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
@@ -20,7 +21,7 @@ public class TaskController {
     private TaskMapper taskMapper;
 
     @RequestMapping(method = RequestMethod.GET, value = "getById/{id}")
-    public TaskDto getById(@PathVariable long id) {
+    public TaskDto getById(@PathVariable Long id) {
         return taskMapper.mapToTaskDto(service.findById(id));
     }
 
@@ -34,8 +35,8 @@ public class TaskController {
         return taskMapper.mapToTaskDto(service.getTask(taskId).orElseThrow(TaskNotFoundException::new));
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
-    public void deleteTask(@RequestParam Long taskId) {
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask/{taskId}")
+    public void deleteTask(@PathVariable Long taskId) {
         service.deleteTask(taskId);
     }
 
@@ -45,7 +46,7 @@ public class TaskController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createTask", consumes = APPLICATION_JSON_VALUE)
-    public void createTask(@RequestBody TaskDto taskDto) {
-        service.saveTask(taskMapper.mapToTask(taskDto));
+    public Task createTask(@RequestBody TaskDto taskDto) {
+        return service.saveTask(taskMapper.mapToTask(taskDto));
     }
 }
